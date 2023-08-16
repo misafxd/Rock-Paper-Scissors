0a68392b466323b0ea22c9540a3c80c350907d4d
misafxd <pisichu.mafs@gmail.com>
2023-08-16 13:30:43 -0600

    Add playRoundfunction

--- index.js
+++ index.js
@@ -9,3 +9,37 @@ function getComputerChoice() {
         break;
     }
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    
+    if (player == computerSelection){
+        return `It's a tie ${player} vs ${computerSelection}`;
+    }else if (player === "rock"){
+        if(computerSelection == "scissors"){
+            return `You win ${player} beats ${computerSelection}!!`
+        } else {
+            return `You lose ${computerSelection} beats ${player}!!`
+        }
+    }else if (player === "scissors"){
+        if(computerSelection == "paper"){
+            return `You win ${player} beats ${computerSelection}!!`
+        } else {
+            return `You lose ${computerSelection} beats ${player}!!`
+        }
+    }else if (player === "paper"){
+        if(computerSelection == "rock"){
+            return `You win ${player} beats ${computerSelection}!!`
+        } else {
+            return `You lose ${computerSelection} beats ${player}!!`
+        }
+    }
+
+    
+}
+
+const pSelection = "rock";
+const cSelection = getComputerChoice();
+
+console.log(playRound(pSelection,cSelection));
